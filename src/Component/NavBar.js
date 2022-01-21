0b955479fa80677f319386c49b27053b08a15a3e
Rashad Long <RDLong718@gmail.com>
2022-01-21 02:14:34 -0500

    Create NavBar.js
    Created skeleton NavBar

---
+++ src/Component/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = (props)=>
+{
+    return(
+        <nav>
+            Link here for Home
+            Link goes here for Campuses
+            Link goes here for Students
+        </nav>
+    )
+}
