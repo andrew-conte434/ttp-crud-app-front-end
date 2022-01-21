102fb70cf55da9a31d096cee1152dc57a1f4bfe3
Steven-C1998 <stevengo123@gmail.com>
2022-01-21 14:58:50 -0500

    Add campus cards component, closes #2

--- src/components/Campus_cards.js
+++ src/components/Campus_cards.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function campus_cards (){
-    
+export default function CampusCards (){
+    const [campusData, setCampusData] = useState([])
+
+    useEffect(() => {
+        (async() => {
+            const res = await fetch(/*api request here*/)
+            const data = await res.json();
+            setCampusData(data)
+        })();
+    },[]);
+
+    return(
+        <div id="campus_card">
+            <img src={campusData.image}></img>
+            <p>{campusData.name}</p>
+            <p>Students...</p>
+        </div>
+    )
 }
