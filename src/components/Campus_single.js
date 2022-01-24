326ba7054673b29fc82f1fd2c482b0a6f775ed84
Steven-C1998 <stevengo123@gmail.com>
2022-01-23 21:12:22 -0500

    closes #9, Added single campus page

---
+++ src/components/Campus_single.js
@@ -0,0 +1,18 @@
+import react from "react";
+
+export default function CampusSingle(props) {
+    return (
+        <div>
+            <h1>Show Campus</h1>
+            {/*navebar*/}
+            <div id="campus_card">
+                <img src={props.image}></img>
+                <h3>{props.name}</h3>
+                <p>{props.description}</p>
+            </div>
+            <div id="campus_address">{props.address}</div>
+            <button id="edit_btn">edit</button>
+            <button id="delete_btn">delete</button>
+        </div>
+    )
+}
